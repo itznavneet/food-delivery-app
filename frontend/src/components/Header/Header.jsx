84f2c435688a6d4d689a0ec206d90ab747b997ab
itznavneet <navypc50@gmail.com>
2026-04-17 07:57:33 +0530

    css header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/frontend_assets/assets'
 
 const Header = () => {
   const handleViewMenu = () => {
@@ -10,7 +11,10 @@ const Header = () => {
   };
 
   return (
-    <div className='header'>
+    <div
+      className='header'
+      style={{ backgroundImage: `url(${assets.header_img})` }}
+    >
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <button onClick={handleViewMenu}>View Menu</button>
